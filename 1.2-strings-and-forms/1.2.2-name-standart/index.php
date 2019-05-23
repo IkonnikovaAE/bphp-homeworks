7043b293addaf5d11dc831e379fdc2c7e9dbc8c0
IkonnikovaAE <lisa_dilan@mail.ru>
2019-05-23 23:43:54 +1000

    first commit

---
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$middleName = $_POST['middleName'];
+
+$fullName = ucfirst($lastName)  . ' ' . ucfirst($firstName) . ' ' . ucfirst($middleName);
+
+$fio = mb_strtoupper(mb_substr($lastName, 0, 1)) . mb_strtoupper(mb_substr($firstName, 0, 1)) . mb_strtoupper(mb_substr($middleName, 0, 1));
+
+$surnameAndInitials = ucfirst($lastName) . ' ' . mb_strtoupper(mb_substr($firstName, 0, 1)) . '.' . mb_strtoupper(mb_substr ($middleName, 0, 1)) . '.';
+
+echo "Полное имя: '$fullName' <br>";
+echo "Фамилия и инициалы: '$fio'  <br>";
+echo "Аббревиатура: '$surnameAndInitials' <br>";
+
