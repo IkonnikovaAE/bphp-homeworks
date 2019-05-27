56150ef500c5cb04ec874bc5242773de6fc0c82e
IkonnikovaAE <lisa_dilan@mail.ru>
2019-05-28 00:21:00 +1000

    second commit

--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -4,11 +4,11 @@ $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $middleName = $_POST['middleName'];
 
-$fullName = ucfirst($lastName)  . ' ' . ucfirst($firstName) . ' ' . ucfirst($middleName);
+$fullName = mb_strtoupper(mb_substr($lastName, 0, 1))  . mb_strtolower(mb_substr($lastName, 1, strlen($lastName))) . ' ' . mb_strtoupper(mb_substr($firstName, 0, 1))  . mb_strtolower(mb_substr($firstName, 1, strlen ($firstName))) . ' ' . mb_strtoupper(mb_substr($middleName, 0, 1))  . mb_strtolower(mb_substr($middleName, 1, strlen ($middleName)));
 
 $fio = mb_strtoupper(mb_substr($lastName, 0, 1)) . mb_strtoupper(mb_substr($firstName, 0, 1)) . mb_strtoupper(mb_substr($middleName, 0, 1));
 
-$surnameAndInitials = ucfirst($lastName) . ' ' . mb_strtoupper(mb_substr($firstName, 0, 1)) . '.' . mb_strtoupper(mb_substr ($middleName, 0, 1)) . '.';
+$surnameAndInitials = mb_strtoupper(mb_substr($lastName, 0, 1))  . mb_strtolower(mb_substr($lastName, 1, strlen($lastName))) . ' ' . mb_strtoupper(mb_substr($firstName, 0, 1)) . '.' . mb_strtoupper(mb_substr ($middleName, 0, 1)) . '.';
 
 echo "Полное имя: '$fullName' <br>";
 echo "Фамилия и инициалы: '$fio'  <br>";
