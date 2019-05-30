3604874fb83de3b273794f702ac5a2197b11c3a0
IkonnikovaAE <lisa_dilan@mail.ru>
2019-05-30 22:18:34 +1000

    second commit

--- 1.1-intro-and-branching/1.1.1/index.php
+++ 1.1-intro-and-branching/1.1.1/index.php
@@ -1,20 +1,26 @@
 <?php
 
 $variable = null;
-$type = gettype($variable);
-switch ($type) :
-    case 'boolean': $description = "Логический тип";
-         break;
-    case 'double': $description = "Число с плавающей точкой <br>Используется для вещественных чисел.";
-         break;
-    case 'integer': $description = "Целое 32-битное число со знаком";
-         break;
-    case 'string': $description = "Строковый тип";
-         break;
-    case 'NULL': $description = "Значение переменной не определено";
-         break;
-    default: $description = "Неизвестный тип";
-endswitch; 
+
+if (is_bool($variable)) {
+    $type = 'bool';
+    $description = "Логический тип";
+} elseif (is_float($variable)) {
+    $type = 'float';
+    $description = "Число с плавающей точкой <br>Используется для вещественных чисел.";
+} elseif (is_int($variable)) {
+    $type = 'int';
+    $description = "Целое 32-битное число со знаком";
+} elseif (is_string($variable)) {
+    $type = 'string';
+    $description = "Строковый тип";
+} elseif (is_null($variable)) {
+    $type = 'null';
+    $description = "Значение переменной не определено";
+} else {
+    $type = 'other';
+    $description = "Неизвестный тип";
+}
 ?>
 
 <!DOCTYPE html>
